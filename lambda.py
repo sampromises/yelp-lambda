d4d5c33caad652103bffb21a70b579932512cfeb
Sam Kim <samkim90274@gmail.com>
2020-03-28 10:04:01 -0700

    Add AWSLambdaBasicExecutionRole to lambda role

--- lambda.py
+++ lambda.py
@@ -1,5 +1,27 @@
 import json
+from dataclasses import dataclass
+
+from typing import Dict
+
+
+@dataclass
+class LambdaJob:
+    items: Dict
+    foo: str
 
 
 def handler(event, context):
-    print("Received event: " + json.dumps(event, indent=2))
+    body = event.get("body")
+    print(f"body: {body}")
+    return {
+        "statusCode": 200,
+        "body": json.dumps(event, indent=2),
+    }
+
+
+def dispatcher_handler(event, context):
+    ...
+
+
+def worker_handler(event, context):
+    ...
--- main.tf
+++ main.tf
@@ -1,45 +1,44 @@
+# Variables
 provider "aws" {
   region = "us-east-1"
 }
 
-variable "function_name" {
-  default = "minimal_lambda_function"
-}
+data "aws_region" "current" {}
 
-variable "handler" {
-  default = "lambda.handler"
-}
+data "aws_caller_identity" "current" {}
 
-variable "runtime" {
-  default = "python3.6"
-}
 
-resource "aws_lambda_function" "lambda_function" {
-  role             = "${aws_iam_role.lambda_exec_role.arn}"
-  handler          = "${var.handler}"
-  runtime          = "${var.runtime}"
-  filename         = "lambda.zip"
-  function_name    = "${var.function_name}"
+# Lambda
+resource "aws_lambda_function" "lambda" {
+  filename = "lambda.zip"
+  function_name = "mylambda"
+  role = "${aws_iam_role.role.arn}"
+  handler = "lambda.handler"
+  runtime = "python3.8"
+
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda.zip"))}"
   source_code_hash = "${filebase64sha256("lambda.zip")}"
 }
 
-resource "aws_iam_role" "lambda_exec_role" {
-  name        = "lambda_exec"
-  path        = "/"
-  description = "Allows Lambda Function to call AWS services on your behalf."
+# IAM
+resource "aws_iam_role" "role" {
+  name = "myrole"
 
-  assume_role_policy = <<EOF
+  assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
       "Principal": {
         "Service": "lambda.amazonaws.com"
       },
-      "Action": "sts:AssumeRole"
+      "Effect": "Allow",
+      "Sid": ""
     }
   ]
 }
-EOF
+POLICY
 }
